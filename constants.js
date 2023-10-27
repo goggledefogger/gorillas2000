14698b96c7dae62945e2d188c03837717678cbe6
townwizard <goggledefogger@users.noreply.github.com>
2023-10-27 14:00:29 -0700

    add a logarithmic factor to the power so it can get more from the maximum

--- constants.js
+++ constants.js
@@ -1,2 +1,4 @@
 const TIME_INCREMENT = 0.1; // Adjust this value to change the simulation speed
 const POWER_FACTOR = 0.5;
+const POWER_SCALE = 10; // Adjust this value to change the sensitivity
+const MAX_TIME = 1000;
--- utilities.js
+++ utilities.js
@@ -8,15 +8,26 @@ function computeTrajectory(game, startX, startY, angle, power, wind, g) {
   let adjustedAngle = game.currentPlayer === 0 ? angle : 180 - angle;
   const radianAngle = radians(adjustedAngle);
 
-  const velocityX = POWER_FACTOR * power * cos(radianAngle);
-  const velocityY = POWER_FACTOR * power * sin(radianAngle);
+  const adjustedPower = adjustPower(power);
 
-  while (yPos < height && yPos > 0) {
+  const velocityX = adjustedPower * cos(radianAngle);
+  const velocityY = adjustedPower * sin(radianAngle);
+
+  while (yPos < height) {
     xPos += (velocityX + wind) * TIME_INCREMENT;
     yPos -= (velocityY - 0.5 * g * time * time) * TIME_INCREMENT;
     trajectory.push({ x: xPos, y: yPos });
     time += TIME_INCREMENT;
+
+    // Safety check to ensure loop doesn't run forever
+    if (time > MAX_TIME) break;
   }
 
   return trajectory;
+}
+
+function adjustPower(power) {
+  const MIN_POWER = 1; // Minimum power value to avoid log(0)
+
+  return Math.log(power + MIN_POWER) * POWER_SCALE;
 }
