9f35cea91582800b2565f75939677d7129233d90
goggledefogger <goggledefogger@users.noreply.github.com>
2023-10-28 10:43:34 -0700

    Update view.js

--- view.js
+++ view.js
@@ -1,10 +1,10 @@
 let bananaImage;
 
 function preload() {
-  bananaImage = loadImage('/images/banana.png', (img) => {
+  bananaImage = loadImage('images/banana.png', (img) => {
     img.resize(30, 0); // Resize to a width of 30 and auto-adjust height to maintain aspect ratio
   });
-  gorillaImageBeforeThrow = loadImage('/images/gorilla-before-throw.png', (img) => {
+  gorillaImageBeforeThrow = loadImage('images/gorilla-before-throw.png', (img) => {
     img.resize(40, 0); // Resize to a width of 40 and auto-adjust height to maintain aspect ratio
   });
 }
