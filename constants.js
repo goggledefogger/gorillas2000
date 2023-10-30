8ce70726bd81e6f1b47dd4a164576cdbd3df08dd
townwizard <goggledefogger@users.noreply.github.com>
2023-10-29 21:29:23 -0700

    improve speed of game and banana rotation

--- constants.js
+++ constants.js
@@ -1,10 +1,13 @@
 const COLLISION_DISTANCE = 35;
 
-const TIME_INCREMENT = 0.1; // Adjust this value to change the simulation speed
+const TIME_INCREMENT = 0.2; // Adjust this value to change the simulation speed
 const POWER_FACTOR = 0.5;
 const POWER_SCALE = 10; // Adjust this value to change the sensitivity
 const MAX_TIME = 1000;
 const PULSE_SPEED = 3; // Adjust for faster or slower pulsating
+
+const ROTATION_ANGLE = 40;
+
 const COLORS = {
   BUILDING: '#000000', // Black
   GORILLA: '#800080', // Purple
