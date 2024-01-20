dd705c56f8ded14732a96d9a82a21b46b13878d0
Danny Bauman <roytownwizard@protonmail.com>
2024-01-20 10:38:06 -0800

    improve building height logic to do clearance better

--- public/constants.js
+++ public/constants.js
@@ -26,6 +26,7 @@ const IMAGE_WIDTHS = {
 };
 
 const BUILDING_WIDTH = 50;
+const BUILDING_MIN_HEIGHT = 100;
 const BUILDING_MAX_HEIGHT_DIFFERENCE = 150;
 const BUILDING_MIN_CLEARANCE = 50; // Minimum clearance above the gorilla for a clear throw
 const BUILDING_MAX_HEIGHT = 325;
